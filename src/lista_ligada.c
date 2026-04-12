ffc24893171c1f00d0616a66df383b98cf91fba3
Uriel Salgado <uri.auss@gmail.com>
2026-04-12 22:40:27 +0000

    Lo que me faltaba

--- src/lista_ligada.c
+++ src/lista_ligada.c
@@ -1,62 +1,83 @@
-#include 'lista_ligada.h'
-#include 'nodo.h'
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lista_ligada.h"
+#include "nodo.h"
 
-DLista* lista_crear(){
-    DLista * l = (DLista*) malloc (sizeof(DLista));
-    if(l != NULL){
-        l -> head = l -> tail = NULL;
+DLista* lista_crear() {
+    DLista *l = (DLista*) malloc(sizeof(DLista));
+    if(l != NULL) {
+        l->head = l->tail = NULL;
     }
     return l;
 }
 
-
-
-int esVacia(DLista* l) { return (l == NULL || l->head == NULL); }
-
-static Nodo* crearNodo(void* dato, size_t size) {
-    Nodo* n = (Nodo*)malloc(sizeof(Nodo));
-    n->dato = malloc(size);
-    memcpy(n->dato, dato, size);
-    n->siguiente = NULL;
-    return n;
+bool lista_es_vacia(DLista *l) { 
+    return (l == NULL || l->head == NULL); 
 }
 
 
+bool lista_insertar_vacia(DLista *l, void *info, size_t size) {
+    if(l == NULL) return false;
+    
+    nodo *nuevo = nodo_crear(info, size);
+    if(!nuevo) return false;
 
-
-bool lista_insertar_vacia(DLista *l, void * info, size_t size){
-    if(l == NULL){
-        return false;
-    }
-    Nodo *n = nodo_crear(info, size);
-    l -> head = l -> tail = n;
+    l->head = l->tail = nuevo;
+    nuevo->sig = nuevo->ant = NULL; 
     return true;
 }
 
-bool lista_insertar_inicio(DLista *l, void * info, size_t size){
-    if(l == NULL){
-        return false;
-    }
-    Nodo *n = nodo_crear(info, size);
-    nuevo -> sig = l -> head;
-    l -> head -> ant = nuevo;
-    l -> head = nuevo; 
-    return true;
-}
-
-bool lista_insertar_fin(DLista *l, void * info, size_t size){
-    if(l == NULL){
-        return false;
-    }
-    Nodo *n = nodo_crear(info, size);
-    nuevo -> sig = l -> tail;
-    l -> tail -> sig = nuevo;
-    l -> tail = nuevo; 
-    return true;
-}
-
-bool lista_insertar_fin(DLista *l, int pos * info, size_t size){
+bool lista_insertar_inicio(DLista *l, void *info, size_t size) {
     if(!l) return false;
-    if(DLista_es_vacia(l))return lista_insertar_vacia(l, info, size);
+    if(lista_es_vacia(l)) return lista_insertar_vacia(l, info, size);
+
+    nodo *nuevo = nodo_crear(info, size);
+    if(!nuevo) return false;
+
+    nuevo->sig = l->head;
+    nuevo->ant = NULL;
+    l->head->ant = nuevo;
+    l->head = nuevo;
+    return true;
 }
 
+bool lista_insertar_fin(DLista *l, void *info, size_t size) {
+    if(!l) return false;
+    if(lista_es_vacia(l)) return lista_insertar_vacia(l, info, size);
+
+    nodo *nuevo = nodo_crear(info, size);
+    if(!nuevo) return false;
+
+    nuevo->ant = l->tail;
+    nuevo->sig = NULL;
+    l->tail->sig = nuevo;
+    l->tail = nuevo;
+    return true;
+}
+
+bool lista_insertar_x_pos(DLista *l, int pos, void *info, size_t size) {
+    if(!l) return false;
+    
+    int n_nodos = lista_num_nodos(l);
+
+    if(lista_es_vacia(l)) return lista_insertar_vacia(l, info, size);
+    if(pos <= 0) return lista_insertar_inicio(l, info, size);
+    if(pos >= n_nodos) return lista_insertar_fin(l, info, size);
+    nodo *nuevo = nodo_crear(info, size);
+    if(!nuevo) return false;
+
+    nodo *tmp = l->head;
+    for(int pos_act = 0; pos_act < pos; pos_act++) {
+        tmp = tmp->sig;
+    }
+    nuevo->ant = tmp->ant;
+    nuevo->sig = tmp;
+    
+    if(tmp->ant != NULL) {
+        tmp->ant->sig = nuevo;
+    }
+    tmp->ant = nuevo;
+
+    return true;
+}
